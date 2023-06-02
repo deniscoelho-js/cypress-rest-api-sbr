520dce36e0821c20de3b58b8574313d529fd161f
Denis Coelho <deniscoelho.dev@gmail.com>
2023-06-01 23:23:36 -0300

    implementando o metodo de login via post

--- cypress/e2e/rest-api/rest-api.cy.js
+++ cypress/e2e/rest-api/rest-api.cy.js
@@ -2,6 +2,16 @@
 
 describe('Rest API', () => {
   it('should create an account', () => {
-    cy.request()
+    cy.request({
+      method: 'POST',
+      url: 'https://barrigarest.wcaquino.me/signin',
+      body: {
+        email: 'denis@email',
+        redirecionar: 'false',
+        senha: 'dd',
+      },
+    })
+      .its('body.token')
+      .should('not.be.empty')
   })
 })
